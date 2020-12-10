083af89ca95f8c015bf105d06cfed0b79e414c08
julegault <legault.julien@gmail.com>
2020-12-10 00:01:04 -0500

    added tests

--- src/test/java/com/hvac/AppTest.java
+++ src/test/java/com/hvac/AppTest.java
@@ -1,5 +1,7 @@
 package com.hvac;
+import com.microsoft.signalr.HubConnectionState;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -9,12 +11,48 @@ import org.junit.Test;
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void setupController() {
+        HVACController hvac = new HVACController(100.0f,0.0f,"https://test.com","/TestHub=","/api/Test/","token");
+        hvac.SetupController();
+        HubConnectionState res = hvac.getConnection().getConnectionState();
+        assertEquals(HubConnectionState.DISCONNECTED, res);
+    }
+
+    @Test
+    public void getMaxTemperature() {
+        HVACController hvac = new HVACController(100.0f,0.0f,"testhost","testurl","testAPI","testtoken");
+        assertEquals(100.0f, hvac.getMaxTemperature(),0.01f );
+    }
+
+    @Test
+    public void getMinTemperature() {
+        HVACController hvac = new HVACController(100.0f,0.0f,"testhost","testurl","testAPI","testtoken");
+        assertEquals(0.0f, hvac.getMinTemperature(),0.01f );
+    }
+
+    @Test
+    public void getHost() {
+        HVACController hvac = new HVACController(100.0f,0.0f,"testhost","testurl","testAPI","testtoken");
+        assertEquals("testhost",hvac.getHost() );
+    }
+
+    @Test
+    public void getHubURL() {
+        HVACController hvac = new HVACController(100.0f,0.0f,"testhost","testurl","testAPI","testtoken");
+        assertEquals("testurl",hvac.getHubURL() );
+    }
+
+    @Test
+    public void getApiURL() {
+        HVACController hvac = new HVACController(100.0f,0.0f,"testhost","testurl","testAPI","testtoken");
+        assertEquals("testAPI",hvac.getApiURL() );
+    }
+
+    @Test
+    public void getToken() {
+        HVACController hvac = new HVACController(100.0f,0.0f,"testhost","testurl","testAPI","testtoken");
+        assertEquals("testtoken",hvac.getToken() );
     }
 }
